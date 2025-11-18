be527175bb9a3a881229ce6fe3cf3d9a814ec947
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-11-18 15:06:45 -0500

    update enviroment

--- ionic-tigoconecta/src/environments/environment.prod.ts
+++ ionic-tigoconecta/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  Supabase_URL:'https://yodfvfwfmiraaargvvye.supabase.co',
+  Supabase_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvZGZ2ZndmbWlyYWFhcmd2dnllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0Mzc3MzQsImV4cCI6MjA3OTAxMzczNH0.PsEd5yheb5j0yBsVQo8vV1QU1HJ5m01qTJ_O3vDSwtA'
+
 };
